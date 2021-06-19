24be93f73623d2270a8397565797fd6fe2b5562e
MariaSemenyuk <u113504907@byui.edu>
2021-06-19 20:49:22 +0200

    Add json to the home page

--- week9/home-page/js/json.js
+++ week9/home-page/js/json.js
@@ -1,5 +1,11 @@
 const jsonURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
+let townName = document.getElementsByClassName('townName');
+let townMotto = document.getElementsByClassName('townMotto');
+let townYear = document.getElementsByClassName('townYear');
+let townPopulation = document.getElementsByClassName('townPopulation');
+let townRain = document.getElementsByClassName('townRain');
+
 fetch(jsonURL)
   .then(function (response) {
     return response.json();
@@ -8,17 +14,10 @@ fetch(jsonURL)
     const towns = jsonObject['towns'];
     // console.table(jsonObject);
     for (let i = 0; i < towns.length; i++ ) {
-      let townName = towns[i].name;
-      let townMotto = towns[i].motto;
-      let townYear = towns[i].yearFounded;
-      let townPopulation = towns[i].currentPopulation;
-      let townRain = towns[i].averageRainfall;
-
-      document.getElementsByClassName('townName').innerHTML = townName;
-
-
-
-
-      // document.querySelector('div.cards').appendChild(card);
+      townName[i].innerHTML = towns[i].name;
+      townMotto[i].innerHTML = towns[i].motto;
+      townYear[i].innerHTML = 'Year Founded: ' + towns[i].yearFounded;
+      townPopulation[i].innerHTML = 'Population: ' + towns[i].currentPopulation;
+      townRain[i].innerHTML = 'Annual Rian Fall: ' + towns[i].averageRainfall;
     }
   });
