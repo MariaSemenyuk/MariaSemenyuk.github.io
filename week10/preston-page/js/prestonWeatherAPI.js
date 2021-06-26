c9766922cd5be36894087c8a575cf812d85dcea3
MariaSemenyuk <u113504907@byui.edu>
2021-06-26 22:32:24 +0200

    Add forecast via API

--- week10/preston-page/js/prestonWeatherAPI.js
+++ week10/preston-page/js/prestonWeatherAPI.js
@@ -1,5 +1,5 @@
 const prestonJSON = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=2ecb8eb95419d049bfa52bf7b8d4678d';
-
+const prestonForesactJSON = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=2ecb8eb95419d049bfa52bf7b8d4678d';
 
 fetch(prestonJSON)
   .then(function (response) {
@@ -26,4 +26,45 @@ fetch(prestonJSON)
     } else {
       document.getElementById('windChill').innerHTML = 'N/A';
     }
+
+  });
+
+
+
+
+  fetch(prestonForesactJSON)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsObject) {
+    // console.table(jsObject);
+    const daysNames = ['Sun', 'Mon', 'Tue', 'Wen', 'Thu', 'Fri', 'Sat'];
+    const today = new Date();
+
+    const newList = jsObject.list.filter(newList =>
+      newList.dt_txt.includes('18:00:00'));
+    
+      for (let i = 0; i < 5; i++) {
+        let index = i + 1;
+        let dayBlock = document.createElement('li');
+        let dayName = document.createElement('span');
+        let dayIcon = document.createElement('img');
+        let dayTemp = document.createElement('span');
+        dayBlock.appendChild(dayName);
+        dayBlock.appendChild(dayIcon);
+        dayBlock.appendChild(dayTemp);
+
+        let dayIndex = today.getDay() + index;
+        if (dayIndex > 6) {
+          dayIndex = dayIndex - 7;
+        }
+        dayName.textContent = daysNames[dayIndex];
+        dayIcon.textContent = newList[i+1].weather[0].icon;
+        dayIcon.setAttribute('src', 'https://openweathermap.org/img/wn/' + newList[index].weather[0].icon + '.png');
+        dayIcon.setAttribute('alt', newList[index].weather[0].description);
+        dayTemp.textContent = (newList[index].main.temp).toFixed(0) + ' °F';
+
+        document.querySelector('.container ul').appendChild(dayBlock);
+
+      }
   });
